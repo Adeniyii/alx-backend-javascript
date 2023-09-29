86442dcf1dd835cfc88e6e96634bc25a44b9680e
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-29 16:28:34 +0100

    make classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+import Classroom from './0-classroom';
+
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+  return new Array(3).fill(0).map((_, i) => Classroom(sizes[i]));
+}
