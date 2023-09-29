da8b751036fee06bdda19d6ab55b979de568c3f5
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-29 23:04:58 +0100

    symbols!!!

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+/* eslint-disable class-methods-use-this */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return 'SFO';
+  }
+}
