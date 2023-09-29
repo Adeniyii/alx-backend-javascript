4fd28b76547e95ceffa35c936a03c40071ea640a
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-29 12:43:08 +0100

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((v) => v.map((vv) => {
-      if (vv.status === 'fulfilled') return vv;
-      return { status: vv.status, value: vv.reason };
-    }));
+// export default async function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+//     .then((v) => v.map((vv) => {
+//       if (vv.status === 'fulfilled') return vv;
+//       return { status: vv.status, value: vv.reason };
+//     }));
+// }
+//
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
