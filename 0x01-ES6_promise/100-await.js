efec3518eb348a50dbef39884462be36d399c97f
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-29 13:48:49 +0100

    handle errors gracefully

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,5 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  return Promise.all([uploadPhoto(), createUser()]).then(([photo, user]) => ({ photo, user }));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => ({ photo, user }))
+    .catch(() => ({ photo: null, user: null }));
 }
