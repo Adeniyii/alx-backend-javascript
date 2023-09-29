4c36660d9c46a800c31d13228b96c5990b132a9f
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-29 16:31:33 +0100

    actually instiantiate the classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,5 +2,5 @@ import Classroom from './0-classroom';
 
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
-  return new Array(3).fill(0).map((_, i) => Classroom(sizes[i]));
+  return new Array(3).fill(0).map((_, i) => new Classroom(sizes[i]));
 }
