a99f29be217daaf0138603e40452a3c1e4e5369a
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-03 19:28:40 +0100

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startStr) {
 
   return Array.from(set).reduce((acc, el, idx) => {
     let prefix = acc;
-    if (startStr.trim() && el.startsWith(startStr)) {
+    if (startStr.trim() && el && el.startsWith(startStr)) {
       if (idx > 0) { prefix += '-'; }
       return prefix + el.slice(startStr.length);
     }
