08d515c1d9d6ee086e678e0ca9914c920a9791a2
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-03 20:10:40 +0100

    silly mistake

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -6,7 +6,7 @@
 export default function updateUniqueItems(items) {
   if (!(items instanceof Map)) throw new Error('Cannot process');
 
-  for (const [k, v] in items.entries()) {
+  for (const [k, v] of items.entries()) {
     if (v === 1) {
       items.set(k, 100);
     }
