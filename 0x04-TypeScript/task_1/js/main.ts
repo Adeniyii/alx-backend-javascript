193473bcd33f636c72f005b7219deb20211e80f5
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-04 18:30:40 +0100

    feat: extending interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
   [value: string]: any
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -14,3 +18,14 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(teacher3);
+console.log(director1);
