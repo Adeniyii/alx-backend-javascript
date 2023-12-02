30ffda9bd54bc3ce49ed8667befe1051fcd57eca
Adeniyii <ifedayoadeniyi@gmail.com>
2023-12-02 17:07:35 +0100

    remove bad formatting

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-function printWelcome () {
+function printWelcome() {
   console.log('Welcome to Holberton School, what is your name?');
   process.stdin.setEncoding('utf8');
-  process.stdin.on('readable', function () {
+  process.stdin.on('readable', function() {
     const chunk = process.stdin.read();
     if (chunk !== null) {
       process.stdout.write('Your name is: ' + chunk);
     }
   });
-  process.on('SIGINT', function () {
+  process.on('SIGINT', function() {
     process.stdout.write('This important software is now closing\n');
     process.exit(0);
   });
