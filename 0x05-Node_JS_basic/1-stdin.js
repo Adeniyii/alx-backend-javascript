f25e9b09c619101c2542445ffee02b7b1d7b844a
Adeniyii <ifedayoadeniyi@gmail.com>
2023-12-02 17:24:04 +0100

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 const printWelcome = () => {
   console.log('Welcome to Holberton School, what is your name?');
   process.stdin.setEncoding('utf8');
-  process.stdin.on('readable', function () {
+  process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
     if (chunk !== null) {
       process.stdout.write('Your name is: ' + chunk);
     }
   });
-  process.stdin.on('end', function () {
+  process.stdin.on('end', () => {
     process.stdout.write('This important software is now closing\n');
     // process.exit(0);
   });
