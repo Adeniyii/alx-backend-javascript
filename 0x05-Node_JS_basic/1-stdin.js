7f965242bbd96488ec42ebac8f61d567aa7ac8e6
Adeniyii <ifedayoadeniyi@gmail.com>
2023-12-02 16:59:32 +0100

    feat: streaming to and from fds

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+function printWelcome () {
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', function () {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+      process.stdout.write('Your name is: ' + chunk);
+    }
+  });
+  process.on('SIGINT', function () {
+    process.stdout.write('This important software is now closing\n');
+    process.exit(0);
+  });
+}
+
+printWelcome();
